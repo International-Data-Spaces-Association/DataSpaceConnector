c9aa1a5f2af565b058c5cc7da966efc3b12233a1
omar <omar.silva@sovity.de>
2022-03-21 15:49:47 +0100

    chore: create daps repository

---
+++ src/main/java/io/dataspaceconnector/repository/DapsRepository.java
@@ -0,0 +1,11 @@
+package io.dataspaceconnector.repository;
+
+import io.dataspaceconnector.model.daps.Daps;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Repository for daps.
+ */
+@Repository
+public interface DapsRepository extends BaseEntityRepository<Daps> {
+}
