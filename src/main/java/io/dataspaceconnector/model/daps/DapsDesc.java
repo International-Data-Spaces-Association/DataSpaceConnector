ded47bfb8914a0245ac64e69b0bbb697a8be870d
omar <omar.silva@sovity.de>
2022-03-21 15:05:42 +0100

    chore: delete not necessary annotations

--- src/main/java/io/dataspaceconnector/model/daps/DapsDesc.java
+++ src/main/java/io/dataspaceconnector/model/daps/DapsDesc.java
@@ -1,15 +1,10 @@
 package io.dataspaceconnector.model.daps;
 
 import io.dataspaceconnector.model.named.NamedDescription;
-import io.dataspaceconnector.model.util.UriConverter;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
-import javax.persistence.Column;
-import javax.persistence.Convert;
 import java.net.URI;
-
-import static io.dataspaceconnector.model.config.DatabaseConstants.URI_COLUMN_LENGTH;
 
 /**
  * Describing DAPS properties.
@@ -21,8 +16,6 @@ public class DapsDesc extends NamedDescription {
     /**
      * The url location of the DAPS.
      */
-    @Convert(converter = UriConverter.class)
-    @Column(length = URI_COLUMN_LENGTH)
     private URI location;
 
 }
