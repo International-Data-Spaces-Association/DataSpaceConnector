2d18c7c4e02f4183a482bdc0d5c9cc89ad643823
omar <omar.silva@sovity.de>
2022-03-22 16:57:32 +0100

    chore: update DapsService to add persisted daps to whitelist

--- src/main/java/io/dataspaceconnector/service/resource/type/DapsService.java
+++ src/main/java/io/dataspaceconnector/service/resource/type/DapsService.java
@@ -15,26 +15,52 @@
  */
 package io.dataspaceconnector.service.resource.type;
 
+import io.dataspaceconnector.config.DapsConfig;
 import io.dataspaceconnector.model.base.AbstractFactory;
 import io.dataspaceconnector.model.daps.Daps;
 import io.dataspaceconnector.model.daps.DapsDesc;
 import io.dataspaceconnector.repository.BaseEntityRepository;
 import io.dataspaceconnector.service.resource.base.BaseEntityService;
+import lombok.NonNull;
 
 /**
  * Service class for daps.
  */
-public class DapsService extends BaseEntityService<Daps, DapsDesc> {
+public class DapsService<T extends Daps, D extends DapsDesc>
+        extends BaseEntityService<T, D> {
 
     /**
-     * Constructor.
-     *
-     * @param repository The daps repository.
-     * @param factory    The daps object logic.
+     * DapsConfig instance used to add DAPS to whitelist
      */
-    public DapsService(final BaseEntityRepository<Daps> repository,
-                         final AbstractFactory<Daps, DapsDesc> factory) {
+    private final @NonNull DapsConfig dapsConfig;
+
+    /**
+     * Constructor for DapsService class, autowires the DapsConfig field.
+     *  @param repository The daps repository.
+     * @param factory    The daps object logic.
+     * @param dapsConfig daps configuration
+     */
+    public DapsService(
+            @NonNull BaseEntityRepository<T> repository,
+            @NonNull AbstractFactory<T, D> factory,
+            @NonNull DapsConfig dapsConfig) {
         super(repository, factory);
+        this.dapsConfig = dapsConfig;
+    }
+
+    /**
+     * Persists a daps and adds it to the whitelist.
+     *
+     * @param daps the daps to persist.
+     * @return the persisted daps.
+     */
+    @Override
+    protected final T persist(final T daps) {
+        if (daps.getLocation() != null) {
+            dapsConfig.addDapsToWhitelist(daps.getLocation());
+        }
+
+        return super.persist(daps);
     }
 
 }
