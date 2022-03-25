84089a13a3e075b1eed5a068e2b1ea1cad8d9bea
omar <omar.silva@sovity.de>
2022-03-25 09:57:26 +0100

    chore: create DapsConfig class to add validation rules to messaging services

--- src/main/java/io/dataspaceconnector/config/DapsConfig.java
+++ src/main/java/io/dataspaceconnector/config/DapsConfig.java
@@ -1,30 +1,56 @@
+/*
+ * Copyright 2022 Sovity Gmbh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.dataspaceconnector.config;
 
 import de.fraunhofer.ids.messaging.core.daps.DapsVerifier;
 import de.fraunhofer.ids.messaging.core.daps.customvalidation.ValidationRuleResult;
+import io.dataspaceconnector.model.daps.Daps;
 import io.dataspaceconnector.repository.DapsRepository;
+import io.jsonwebtoken.Claims;
 import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Configuration;
 
 import java.net.URI;
+import java.util.List;
 
+@Log4j2
 @Configuration
 public class DapsConfig {
 
     public final @NonNull DapsRepository repository;
 
     public DapsConfig(@NonNull DapsRepository repository) {
+        checkWhitelist();
         this.repository = repository;
     }
 
-    public void addDapsToWhitelist(URI location) {
-        System.out.println("Adding DAPS: " + location + " to whitelist.");
+    public void checkWhitelist() {
         DapsVerifier.addValidationRule(claim -> {
-                    if (location.toString().equals(claim.getIssuer())) {
+            log.info("Issuer; " + claim.getIssuer());
+                    if (isWhitelisted(claim)) {
                         return ValidationRuleResult.success();
                     }
                     return ValidationRuleResult.failure("Issuer DAPS not whitelisted");
                 }
         );
+    }
+
+    private boolean isWhitelisted(Claims claim) {
+        List<String> whitelistedDaps = repository.findAll().stream().map(Daps::getLocation).map(URI::toString).toList();
+        return whitelistedDaps.size() == 0 || whitelistedDaps.contains(claim.getIssuer());
     }
 }
