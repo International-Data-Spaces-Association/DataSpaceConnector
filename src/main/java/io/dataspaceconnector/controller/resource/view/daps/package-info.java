cddde0304b1b38b453d685e4711a559e29f0c7c9
Sebastian Opriel <sebastian.opriel@sovity.de>
2022-03-31 21:31:05 +0200

    chore: adjusted copyright header

--- src/main/java/io/dataspaceconnector/controller/resource/view/daps/package-info.java
+++ src/main/java/io/dataspaceconnector/controller/resource/view/daps/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 Sovity Gmbh
+ * Copyright 2022 sovity GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
