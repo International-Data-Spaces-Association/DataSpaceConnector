63f7ffb80a0e63190afea99c027a3f785de5a893
Tim Berthold <tim.berthold@sovity.de>
2022-04-14 14:55:57 +0200

    chore: add db migration sql

---
+++ src/main/resources/db/migration/V7.0.0__releaseV8.0.0.sql
@@ -0,0 +1,17 @@
+ALTER TABLE public.agreement
+    RENAME COLUMN value to agreement_value;
+
+ALTER TABLE public.data
+    RENAME COLUMN value to localdata_value;
+
+ALTER TABLE public.authentication
+    RENAME COLUMN key to api_key;
+
+ALTER TABLE public.authentication
+    RENAME COLUMN value to api_value;
+
+ALTER TABLE public.route
+    RENAME COLUMN end to route_end;
+
+ALTER TABLE public.contractrule
+    RENAME COLUMN value to contractrule_value;
