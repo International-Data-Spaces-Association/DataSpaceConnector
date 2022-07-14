defb68c6b21c44a2b14d594a7638149807e0df7e
omar <omar.silva@sovity.de>
2022-07-14 10:51:38 +0200

    chore: add test

--- src/test/java/io/dataspaceconnector/common/net/HttpServiceTest.java
+++ src/test/java/io/dataspaceconnector/common/net/HttpServiceTest.java
@@ -229,4 +229,40 @@ class HttpServiceTest {
         assertEquals(responseCode, result.getCode());
         assertArrayEquals(bytes, result.getData().readAllBytes());
     }
+
+    @Test
+    @SneakyThrows
+    void get_withAlreadyDefinedQuery() {
+        /* ARRANGE */
+        final var params = Map.of("name", "doe");
+        final var args = new HttpService.HttpArgs();
+        args.setParams(params);
+
+        final var target = new URL("https://target.com/?name=john");
+
+        final var response = new Response.Builder()
+                .request(new Request.Builder().url(target).build())
+                .protocol(Protocol.HTTP_1_1).code(200).message("Some message")
+                .body(ResponseBody.create("someBody", MediaType.parse("application/text")))
+                .build();
+
+        // The first response will be consumed by the first request, duplicate
+        final var response2 = new Response.Builder()
+                .request(new Request.Builder().url(target).build())
+                .protocol(Protocol.HTTP_1_1).code(200).message("Some message")
+                .body(ResponseBody.create("someBody", MediaType.parse("application/text")))
+                .build();
+
+        Mockito.doReturn(response).when(httpSvc).get(any());
+
+        /* ACT */
+        final var result = (HttpResponse) service.request(HttpService.Method.GET, target, args);
+
+        /* ASSERT */
+        Mockito.doReturn(response2).when(httpSvc).get(any());
+        final var expected = (HttpResponse) service.get(target, args);
+        assertEquals(expected.getCode(), result.getCode());
+        assertTrue(Arrays.areEqual("someBody".getBytes(StandardCharsets.UTF_8),
+                result.getData().readAllBytes()));
+    }
 }
